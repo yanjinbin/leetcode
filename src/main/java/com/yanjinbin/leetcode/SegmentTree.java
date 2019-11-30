b77cb894f5dc07c2ea28f9fb99ad8504233908a5
yanjinbin <yanjinbin@qq.com>
2019-12-01 00:52:11 +0800

    线段树和ST表

---
+++ src/main/java/com/yanjinbin/leetcode/SegmentTree.java
@@ -0,0 +1,67 @@
+package com.yanjinbin.leetcode;
+
+import lombok.AllArgsConstructor;
+
+// https://oi-wiki.org/ds/seg/
+// 307 315
+public class SegmentTree {
+
+    @AllArgsConstructor
+    public class SegmentTreeNode {
+        int start, end;
+        int sum;//can be max min
+        SegmentTreeNode left;
+        SegmentTreeNode right;
+
+        public SegmentTreeNode(int start, int end, int sum) {
+            this.start = start;
+            this.end = end;
+            this.sum = sum;
+        }
+    }
+
+    public SegmentTreeNode root;
+
+    public SegmentTree(int[] nums) {
+        this.root = buildTree(0, nums.length - 1, nums);
+    }
+
+    public SegmentTreeNode buildTree(int start, int end, int[] vals) {
+        if (start == end) return new SegmentTreeNode(start, end, vals[start]);
+        int mid = (end - start) / 2 + start;
+        SegmentTreeNode left = buildTree(start, mid, vals);
+        SegmentTreeNode right = buildTree(mid + 1, end, vals);
+        return new SegmentTreeNode(start, end, left.sum + right.sum, left, right);
+    }
+
+
+    public void updateTree(SegmentTreeNode root, int i, int val) {
+        if (root.start == i && root.end == i) {
+            root.sum = val;
+            return;
+        }
+        int mid = (root.end - root.start) / 2 + root.start;
+        if (i <= mid) {
+            updateTree(root.left, i, val);
+        } else {
+            updateTree(root.right, i, val);
+        }
+        // 别忘记update!!!
+        root.sum = root.left.sum + root.right.sum;
+    }
+
+    public int sumRange(SegmentTreeNode root, int i, int j) {
+        if (root.start == i && j == root.end) {
+            return root.sum;
+        }
+        int mid = (root.end - root.start) / 2 + root.start;
+        if (j <= mid) {
+            return sumRange(root.left, i, j);
+        } else if (mid > i) {
+            return sumRange(root.right, i, j);
+        } else {
+            // 别搞错mid+1放哪里哦
+            return sumRange(root.left, i, mid) + sumRange(root.right, mid + 1, j);
+        }
+    }
+}
---
+++ src/main/java/com/yanjinbin/leetcode/SparseTable.java
@@ -0,0 +1,27 @@
+package com.yanjinbin.leetcode;
+
+// https://oi-wiki.org/ds/sparse-table/
+// [tag：高级数据结构]
+// https://www.cnblogs.com/zwfymqz/p/8581995.html
+// https://zh.wikipedia.org/wiki/%E8%8C%83%E5%9B%B4%E6%9C%80%E5%80%BC%E6%9F%A5%E8%AF%A2
+public class SparseTable {
+    int N = 1000;
+    int[][] dp = new int[1000][];
+    //O(NlogN)
+    public void Build(int N, int M, int[] nums) {
+
+        for (int i = 1; i <= N; i++) {
+            dp[i][0] = nums[i];
+        }
+        for (int j = 1; j <= 21; j++) {
+            for (int i = 1; i + (1 << j) - 1 <= N; j++) {
+                dp[i][j] = Math.max(dp[i][j - 1], dp[i + (1 << (j - 1))][j - 1]);
+            }
+        }
+    }
+    //O(1)
+    public int Query(int l, int r) {
+        int k = (int) Math.log(r - l + 1);
+        return Math.max(dp[l][k], dp[r - (1 << k) + 1][k]);
+    }
+}
