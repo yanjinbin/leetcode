29a4a8e46627476fe71842af9f88b2b947a9b7f3
yanjinbin <yanjinbin@qq.com>
2019-12-11 23:24:33 +0800

    778 水位上升的游泳池游泳 BFS+优先队列/BFS+二分搜索

--- src/main/java/com/yanjinbin/leetcode/MonotoneStack.java
+++ src/main/java/com/yanjinbin/leetcode/MonotoneStack.java
@@ -7,6 +7,7 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map;
 import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.Stack;
 
 public class MonotoneStack {
@@ -98,8 +99,7 @@ public class MonotoneStack {
         return res;
     }
 
-
-    // 407 接雨水Ⅱ 优先队列+BFS
+    // 407 接雨水Ⅱ 优先队列+BFS https://youtu.be/7niUr7LlviY
     public int trapRainWater(int[][] heightMap) {
         PriorityQueue<Tuple> q = new PriorityQueue<Tuple>((o1, o2) -> o1.z - o2.z);
         int m = heightMap.length;
@@ -129,7 +129,7 @@ public class MonotoneStack {
                 int ny = dy[i] + y;
                 if (nx >= 0 && nx < m && ny >= 0 && ny < n && !visited[nx][ny]) {
                     visited[nx][ny] = true;
-                    if (height > heightMap[nx][ny]) {
+                    if (height > heightMap[nx][ny]) {// 注意q存储的 tuple的height 是height还是heightMap[nx][ny]
                         water += height - heightMap[nx][ny];
                         q.offer(new Tuple(nx, ny, height));
                     } else {
@@ -141,13 +141,84 @@ public class MonotoneStack {
         return water;
     }
 
-    // 778
+    // https://youtu.be/umdk98ynLSY
+    // 778 解法1 dijkstra/优先队列+BFS ,总结:最大里面取最小!!(从小堆里面取留下的大元素)
+    public int swimInWater01(int[][] grid) {
+        PriorityQueue<Point> q = new PriorityQueue<Point>((o1, o2) -> o1.val - o2.val);
+        int N = grid.length;
+        boolean[] visited = new boolean[N * N];
+        q.offer(new Point(0, 0, grid[0][0]));
+        // x*n+y
+        visited[0] = true;
+        int[] dirs = new int[]{-1, 0, 1, 0, -1};
+        while (!q.isEmpty()) {
+            Point p = q.poll();
+            if (p.x == N - 1 && p.y == N - 1) return p.val;
+            for (int i = 0; i < 4; i++) {
+                int nx = p.x + dirs[i];
+                int ny = p.y + dirs[i + 1];
+                if (nx >= N || nx < 0 || ny >= N || ny < 0 || visited[nx * N + ny]) continue;
+                visited[nx * N + ny] = true;
+                //注意是取max哦.因为heap是minHeap
+                q.offer(new Point(nx, ny, Math.max(grid[nx][ny], p.val)));
+            }
+        }
+        return -1;
+    }
+
+    // 778 解法2 bfs+二分查找
+    public int swimInWater02(int[][] grid) {
+        int lo = 0, hi = grid.length * grid.length;
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (hasPath(mid, grid.length, grid)) {
+                hi = mid;
+            } else {
+                lo = mid + 1;
+            }
+        }
+        return lo;
+
+    }
+
+    public boolean hasPath(int t, int N, int[][] grid) {
+        if (grid[0][0] > t) return false;
+        Queue<Integer> q = new LinkedList<>();
+        boolean[] seen = new boolean[N * N];
+        int[] dirs = new int[]{-1, 0, 1, 0, -1};
+        q.add(0);
+        while (!q.isEmpty()) {
+            int idx = q.poll();
+            int x = idx % N;
+            int y = idx / N;
+            if (x == N - 1 && y == N - 1) return true;
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dirs[i];
+                int ny = y + dirs[i + 1];
+                if (nx < 0 || ny < 0 || nx >= N || ny >= N || grid[nx][ny] > t) continue;
+                int ndx = ny * N + nx;
+                if (seen[ndx]) continue;
+                seen[ndx] = true;
+                q.add(ndx);
+            }
+        }
+        return false;
+
+    }
+
+
+    public static void main(String[] args) {
+        PriorityQueue<Point> q = new PriorityQueue<Point>((o1, o2) -> o1.val - o2.val);
+        q.offer(new Point(1, 1, 3));
+        q.offer(new Point(1, 2, 4));
+        q.offer(new Point(1, 4, 1));
+        System.out.println(q.peek().val);
+    }
+
 
     // 1263 BFS+优先队列
 
     // 943 花花酱视频 https://youtu.be/u_Wc4jwrp3Q
-
-
     // https://www.cnblogs.com/neopenx/p/4023458.html
     // https://www.ctolib.com/topics-53827.html
 
