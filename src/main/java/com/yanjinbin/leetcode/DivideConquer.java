206b0b7c18437a8da8c9290b79bf1074c294d5c0
yanjinbin <yanjinbin@qq.com>
2019-12-17 02:26:44 +0800

    LC 4 寻找中位数 通用解 find kth in two sorted array.

--- src/main/java/com/yanjinbin/leetcode/DivideConquer.java
+++ src/main/java/com/yanjinbin/leetcode/DivideConquer.java
@@ -6,32 +6,34 @@ public class DivideConquer {
     // 4 两个有序数组的中位数 https://youtu.be/KB9IcSCDQ9k
     // tips: 看懂哪个模型即可 m1-1,m1, m2-1,m2
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int m = nums1.length;
-        int n = nums2.length;
-        if (m > n) return findMedianSortedArrays(nums2, nums1);
-        int k = (m + n + 1) / 2;
-        int l = 0, r = m;
-        while (l < r) {
-            int m1 = (r - l) / 2 + l;
-            int m2 = k - m1;
-            if (nums1[m1] < nums2[m2 - 1]) {
-                // nums1[m1]是没有选到的,nums2[m2-1]是选到的 如果没被选到的<被选到的,那么L增大
-                l = m1 + 1;
-            } else {
-                r = m1;
-            }
+        int n = nums1.length;
+        int m = nums2.length;
+        int left = (n + m + 1) / 2;
+        int right = (n + m + 2) / 2;
+        //将偶数和奇数的情况合并，如果是奇数，会求两次同样的 k 。
+        int i= getKth(nums1, 0, n - 1, nums2, 0, m - 1, left);
+        int j= getKth(nums1, 0, n - 1, nums2, 0, m - 1, right);
+        return (i+j) * 0.5;
+    }
+
+    public int getKth(int[] nums1, int start1, int end1, int[] nums2, int start2, int end2, int k) {
+        int len1 = end1 - start1 + 1;
+        int len2 = end2 - start2 + 1;
+        //让 len1 的长度小于 len2，这样就能保证如果有数组空了，一定是 len1
+        if (len1 > len2) return getKth(nums2, start2, end2, nums1, start1, end1, k);
+        if (len1 == 0) return nums2[start2 + k - 1];
+
+        if (k == 1) return Math.min(nums1[start1], nums2[start2]);
+
+        // 不然的话i或者j会有可能变为负数。
+        int i = start1 + Math.min(len1, k / 2) - 1;
+        int j = start2 + Math.min(len2, k / 2) - 1;
+        if (nums1[i] > nums2[j]) {
+            return getKth(nums1, start1, end1, nums2, j + 1, end2, k - (j - start2 + 1));
         }
-        int m1 = l;
-        int m2 = k - l;
-        int c1 = Math.max(
-                m1 <= 0 ? Integer.MIN_VALUE : nums1[m1 - 1],
-                m2 <= 0 ? Integer.MIN_VALUE : nums2[m2 - 1]
-        );
-        if ((m + n) % 2 == 1) {
-            return c1;
+        else {
+            return getKth(nums1, i + 1, end1, nums2, start2, end2, k - (i - start1 + 1));
         }
-        int c2 = Math.min(m1 >= m ? Integer.MAX_VALUE : nums1[m1], m2 >= n ? Integer.MAX_VALUE : nums2[m2]);
-        return (c1 + c2) * 0.5;
     }
 
     //②  [tag:微软面筋] https://www.1point3acres.com/bbs/thread-541121-1-1.html
