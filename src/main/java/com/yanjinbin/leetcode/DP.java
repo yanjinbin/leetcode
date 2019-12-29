7fe6f429e3769b12bb3bd297fcac493d43fcbfe5
yanjinbin <yanjinbin@qq.com>
2019-12-29 19:34:11 +0800

    LC 265 粉刷房子Ⅱ https://youtu.be/a6rDSwdW2Mo?t=1116 tips: 当前房间刷成j色成本 = (j色和上一间房间最小成本的颜色min1相同的话, 取上一件房间第二小成本的颜色Min2,否则取min1

--- src/main/java/com/yanjinbin/leetcode/DP.java
+++ src/main/java/com/yanjinbin/leetcode/DP.java
@@ -2,52 +2,35 @@ package com.yanjinbin.leetcode;
 
 // dp专题 背包问题九讲
 public class DP {
-    // P428  最大连续和 问题等同于 [leetcode 53 最大子序和]
-    // 解法1 暴力枚举
-    public int maxConsecutiveSum1(int[] arr) {
-        int tot = 0;
+
+    // P428  最大连续和 问题等同于 LC 53 最大子序和
+    // 复杂度 T:O(n),S:O(N)
+    public int maxConsecutiveSum01(int[] arr) {
         int N = arr.length;
-        int best = arr[0];
-        for (int i = 0; i < N; i++) {
-            for (int j = i; j < N; j++) {
-                int sum = 0;
-                for (int k = i; k <= j; k++) {
-                    sum = sum + arr[k];
-                    tot++;
-                }
-                if (sum > best) best = sum;
-            }
+        int[] dp = new int[N];
+        int ans = arr[0];
+        dp[0] = arr[0];
+        for (int i = 1; i < N; i++) {
+            dp[i] = Math.max(dp[i - 1] + arr[i], arr[i]);
+            ans = Math.max(ans, dp[i]);
         }
-        System.out.println("运算次数:\t" + tot);
-        return best;
+        return ans;
     }
 
-    //解法2 写出递推公式 Max(arr[i:j]) = S[0:j]-S[0:i-1]
-    public int maxConsecutiveSum2(int[] arr) {
-        int tot = 0;
-        int[] S = new int[arr.length + 1];
-        int N = S.length;
-        //init
-        S[0] = 0;
-
-        // for
+    // 滚动数组优化  T:O(N) S:O(1)
+    public int maxConsecutiveSum02(int[] arr) {
+        int N = arr.length;
+        int ans = arr[0];
+        int prev = arr[0];
         for (int i = 1; i < N; i++) {
-            S[i] = S[i - 1] + arr[i - 1];
-            tot++;
+            prev = Math.max(prev + arr[i], arr[i]);
+            ans = Math.max(ans, prev);
         }
-
-        int best = S[0];
-        for (int i = 1; i < N; i++) {
-            for (int j = i; j < N; j++) {
-                best = Math.max(best, S[j] - S[i - 1]);
-                tot++;
-            }
-        }
-        return best;
+        return ans;
     }
 
-    //解法3 分治法
-    public int maxConsecutiveSum3(int[] arr) {
+    //解法3 分治法 T: O(lgN)
+    public int maxConsecutiveSum03(int[] arr) {
         return maxSum(arr, 0, arr.length);
     }
 
@@ -73,12 +56,6 @@ public class DP {
         return Math.max(maxs, L + R);
     }
 
-    // chapter 8.3  递归与分治
-
-
-    // 514 自由之路 https://leetcode-cn.com/problems/freedom-trail/
-
-
     //256 粉刷房子 paint house
     public int minCost(int[][] costs) {
         int N = costs.length;
@@ -87,16 +64,43 @@ public class DP {
         dp[0][1] = costs[0][1];
         dp[0][2] = costs[0][2];
         for (int i = 1; i < N; i++) {
-            dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2])+costs[i][0];
-            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2])+costs[i][1];
-            dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1])+costs[i][2];
+            dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + costs[i][0];
+            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + costs[i][1];
+            dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + costs[i][2];
         }
         return Math.min(Math.min(dp[N - 1][0], dp[N - 1][1]), dp[N - 1][2]);
     }
 
-    // 265 粉刷房子2
-    public int minCostⅡ(int[][] costs){
-        return -1;
+    // 265 粉刷房子Ⅱ https://youtu.be/a6rDSwdW2Mo?t=1116
+    // tips: 当前房间刷成j色成本 = (j色和上一间房间最小成本的颜色min1相同的话,
+    // 取上一件房间第二小成本的颜色Min2,否则取min1
+    public int minCostⅡ(int[][] cost) {
+        if (cost == null || cost.length == 0) return 0;
+        int n = costs.length, k = costs[0].length;
+        int[][] dp = new int[n][m];
+        int min1 = -1, min = -2;// min1 和 min2代表刷成min1和min2颜色的成本第一小和第二小
+        for (int i = 0; i < n; i++) {
+            // 暂时存储上一次存储的颜色
+            int last1 = min1, last2 = min2;
+            min1 = -1;
+            min2 = -1;
+            // 比对当前房间和上间房间刷成的颜色相同和不同的成本
+            for (int j = 0; j < k; j++) {
+                if (j != last1) {// 颜色不同
+                    dp[i][j] = cost[i][j] + last1 < 0 ? 0 : dp[i - 1][last1];
+                } else {// 颜色相同
+                    dp[i][j] = last2 < 0 ? dp[i - 1][last2] + cost[i][j];
+                }
+                if (min1 < 0 || dp[i][j] < dp[i][min1]) { // 当前刷成j色的成本比之前的min1开销成本还低
+                    min2 = min1;
+                    min1 = j;
+                } else if (min2 < 0 || dp[i][j] < dp[i][min2]) {// 介于 min1和min2志坚
+                    min2 = j;
+                }
+            }
+        }
+        return dp[n - 1][min1];
     }
+
 
 }
